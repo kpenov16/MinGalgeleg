0aeff0af3df26049f18d2f7ddea772945bcc4a76
kpenov16 <s133967@student.dtu.dk>
2020-10-11 22:36:05 +0200

    add state transition table and some methods to implement

---
+++ app/src/main/java/dk/kaloyan/fsm/TurnstileFSM.java
@@ -0,0 +1,32 @@
+package dk.kaloyan.fsm;
+
+public abstract class TurnstileFSM {
+    private TurnstileState state;
+    public void start() {
+        state.start(this);
+    }
+    public void guess() {
+        state.guess(this);
+    }
+    public void back(){
+        state.back(this);
+    }
+
+    //| current state | event | new state |      action         |
+    //|=========================================================|
+    //|    init       | back  |     -     |         -           |
+    //|    init       | start | playing   | show current game   |
+    //|    playing    | back  | init      | show last top score |
+    //|    playing    | guess | playing   | show current game   |
+    //|    playing    | guess | win       | show you win        |
+    //|    win        | back  | init      | show new top score  |
+    //|    playing    | guess | lose      | show you lose       |
+    //|    lose       | back  | init      | show last top score |
+    //|=========================================================|
+
+    public abstract void showCurrentGame();
+    public abstract void showLastTopScore();
+    public abstract void showYouLose();
+    public abstract void showYouWin();
+    public abstract void showTopScores();
+}
---
+++ app/src/main/java/dk/kaloyan/fsm/TurnstileState.java
@@ -0,0 +1,16 @@
+package dk.kaloyan.fsm;
+
+public class TurnstileState {
+    public void start(TurnstileFSM turnstileFSM) {
+
+    }
+
+    public void guess(TurnstileFSM turnstileFSM) {
+
+    }
+
+    public void back(TurnstileFSM turnstileFSM) {
+
+    }
+
+}
