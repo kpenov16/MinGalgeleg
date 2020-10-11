b923684d66cbbe4a5a17fbca885fd99629113622
kpenov16 <s133967@student.dtu.dk>
2020-10-11 12:10:32 +0200

    add simple start activity to show scores and start the game

---
+++ app/src/main/java/dk/kaloyan/mingalgeleg/StartActivity.java
@@ -0,0 +1,38 @@
+package dk.kaloyan.mingalgeleg;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StartActivity extends AppCompatActivity {
+
+    private ListView listViewScore;
+    private TextView textViewListElement;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        init();
+
+
+        String[] scores = new String[]{"kaloyan: score 5","bob: score 3"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.highscore_list_element, R.id.textViewListElement, scores);
+        listViewScore.setAdapter(adapter);
+
+    }
+
+    private void init() {
+        listViewScore = findViewById(R.id.listViewScore);
+        textViewListElement = findViewById(R.id.textViewListElement);
+
+
+    }
+}
